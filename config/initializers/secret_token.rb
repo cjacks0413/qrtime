01aa0ccc179fad01886f3358d24300c3e1db2e51
Ben Shapiro <ben@getdown.org>
2013-02-12 19:39:58 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrtime::Application.config.secret_token = '7864e644610b87f91f10d9a76f410f6004a9da7454ce9f710bd7b65ca9b0e445a2dbb2dcf82b7437cfd2cc30dba600a686da985d2694377d436aedef9074065e'
