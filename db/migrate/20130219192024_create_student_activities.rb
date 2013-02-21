6847bf2d90495133160b646ea51959a7ec65b573
Ben Shapiro <ben@getdown.org>
2013-02-21 13:06:08 -0500

    remove spurious drop table... put those in a down from now on

--- db/migrate/20130219192024_create_student_activities.rb
+++ db/migrate/20130219192024_create_student_activities.rb
@@ -1,13 +1,11 @@
 class CreateStudentActivities < ActiveRecord::Migration
   def change
-    drop_table :student_activities
     create_table :student_activities do |t|
       t.integer :user_id
       t.integer :course_session_id
       t.string :action
       t.date :date
       t.time :time
-
       t.timestamps
     end
   end
