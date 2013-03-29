f9a48159a552a7a42811de1bd2e45938df431e3e
Sabrina Gordon <sabrina.r.gordon@gmail.com>
2013-03-29 15:44:04 -0400

    only course sessions taught by instructor appear in calendar, admin and insturctor see QR code at check-in/out page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,11 @@ class HomeController < ApplicationController
     @user = current_user
     if current_user.has_role?(:instructor) == true
        @courses = Course.all(:conditions => {:instructor => current_user.id})
+       @course_sessions = CourseSession.all(:conditions => {:course_id => @courses})
     elsif current_user.has_role?(:student) == true
       @courses = @user.courses
     end
-    @course_sessions = CourseSession.all
+
     @student_activities = StudentActivity.find_all_by_user_id(@user)
     @caldate = params[:month] ? Date.parse(params[:month]) : Date.today
   end
--- app/models/course_session.rb
+++ app/models/course_session.rb
@@ -1,6 +1,6 @@
 class CourseSession < ActiveRecord::Base
   resourcify
-  attr_accessible :course_id, :date, :end_time, :start_time, :description, :check_in_url, :check_out_url, :counter
+  attr_accessible :course_id, :date, :end_time, :start_time, :description, :check_in_url, :check_out_url
 
   validates_presence_of :course_id, :date, :end_time, :start_time
   belongs_to :course
