91998b482081fdf6a07af0270edc7c8032a44e30
cjacks0413 <chjackson93@gmail.com>
2014-05-04 11:31:36 -0400

    concatenate qualtrics url

--- app/models/course_session.rb
+++ app/models/course_session.rb
@@ -1,8 +1,14 @@
 class CourseSession < ActiveRecord::Base
+  after_create :set_checkout 
   resourcify
   attr_accessible :course_id, :date, :end_time, :start_time, :description, :check_in_url, :check_out_url
 
   validates_presence_of :course_id, :date, :end_time, :start_time
   belongs_to :course
   has_many :student_activities
+
+  def set_checkout 
+  	self.check_in_url = "https://tufts.qualtrics.com/SE/?SID=SV_54lHEJAekyaMic5&Speaker=#{self.course.instructor}&Date=#{self.date.strftime("%m-%Y")}&Title=#{self.course.name}"
+  end 
+
 end
